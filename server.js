17c190edaed55f01c170d82677bf96b71ff4aa12
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-12-08 18:43:19 -0600

    debug

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const html_route = require('./routes/html-route');
+const api_route = require('./routes/api-route');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(html_route);
+app.use(api_route);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
